1c2e438f087954fea1862f71c2b2701e3321f4fe
sapar <sapar@meyvndigital.co.uk>
2021-10-07 22:56:28 +0300

    [task 1/4]: arrays solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,13 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_num = array.sort[-1]
+        array.map(&:to_i).map{|num| num > 0 ? num = max_num : num}
       end
 
       def search(_array, _query)
-        0
+        _array.map.with_index{|num, index| return index if num == _query}
+        return -1
       end
     end
   end
