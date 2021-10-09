ae754996430ff52c7abe9740c59af72e186e1fe6
sapar <sapar@meyvndigital.co.uk>
2021-10-09 23:42:12 +0300

    [task 3/4]: done

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,43 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        i = 0
+        while i < self.length
+          yield self[i]
+          i+= 1
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        if block_given?
+          self.my_each {|el| result << yield(el)}
+        end
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        self.my_each{|el| result << el unless el.nil?}
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc=nil, &block)
+        _array = self
+        
+        unless acc.present?
+          acc = self[0] # first element taken as acc
+          _array = self.drop(1) # don't count first element
+        end
+
+        _array.my_each {|el| acc = yield(acc, el)}
+        acc
+      end
     end
   end
 end
