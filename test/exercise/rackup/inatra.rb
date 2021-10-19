9775fbac14e3094563e6a4278c78db9b7c4ffdb7
sapar <sapar@meyvndigital.co.uk>
2021-10-19 22:37:55 +0300

    [task 4/4] solve rackup puzzle

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,23 @@
 module Inatra
   class << self
-    def routes(&block); end
+    def routes(&block) 
+      @current_methods = Hash.new
+      @current_methods.default = proc{|hash, key| [404, {}, ['Not Found']]}
+      instance_eval &block
+    end
 
-    def call(env); end
+    [:get, :post].each do |method|
+      define_method(method) do |my_arg, &blk|
+        if blk
+          @current_methods[[method, my_arg]] = blk
+        else
+          @current_methods[[method, my_arg]].call
+        end
+      end
+    end
+
+    def call(env)
+      send(env["REQUEST_METHOD"].downcase.to_sym, env["PATH_INFO"])
+    end
   end
 end
