0d164fa3ed4e931d1dd8e33500b6dabc3358fb9e
sapar <sapar@meyvndigital.co.uk>
2021-10-19 23:05:12 +0300

    fixes according to rubocop report

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,12 +1,12 @@
 module Inatra
   class << self
-    def routes(&block) 
-      @current_methods = Hash.new
-      @current_methods.default = proc{|hash, key| [404, {}, ['Not Found']]}
-      instance_eval &block
+    def routes(&block)
+      @current_methods = {}
+      @current_methods.default = proc { |_hash, _key| [404, {}, ['Not Found']] }
+      instance_eval(&block)
     end
 
-    [:get, :post].each do |method|
+    %i[get post].each do |method|
       define_method(method) do |my_arg, &blk|
         if blk
           @current_methods[[method, my_arg]] = blk
@@ -17,7 +17,7 @@ module Inatra
     end
 
     def call(env)
-      send(env["REQUEST_METHOD"].downcase.to_sym, env["PATH_INFO"])
+      send(env['REQUEST_METHOD'].downcase.to_sym, env['PATH_INFO'])
     end
   end
 end
