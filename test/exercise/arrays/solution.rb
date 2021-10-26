de534aba22ca2b308f3619cd29c4e5d766b0ea83
sapar <sapar@meyvndigital.co.uk>
2021-10-26 18:47:54 +0300

    recursive binary search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,9 +6,25 @@ module Exercise
         array.map(&:to_i).map { |num| num > 0 ? num = max_num : num }
       end
 
-      def search(array, query)
-        array.map.with_index { |num, index| return index if num == query }
-        -1
+      def search(array, query, start_index = nil, end_index = nil)
+        start_index ||= 0
+        end_index ||= array.length - 1
+
+        return -1 if start_index > end_index
+
+        middle_index = (start_index + end_index) / 2
+        case query <=> array[middle_index]
+        when 0
+          middle_index
+        when -1
+          max_index = middle_index - 1
+          search(array, query, start_index, middle_index - 1)
+        when 1
+          min_index = middle_index + 1
+          search(array, query, middle_index + 1, end_index)
+        else
+          -1
+        end
       end
     end
   end
