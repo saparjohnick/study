774b5c469db48a5faca8847eb01eff2976517fe1
sapar <sapar@meyvndigital.co.uk>
2021-10-30 15:02:36 +0300

    redo solution with recursion

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,40 +5,43 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        i = 0
-        while i < length
-          yield self[i]
-          i += 1
-        end
-        self
+      def my_each(i = nil, &block)
+        i ||= 0
+        return self unless i < length
+
+        block.call(self[i])
+        my_each(i += 1, &block)
       end
 
       # Написать свою функцию my_map
-      def my_map
+      def my_map(&block)
         result = MyArray.new
-        my_each { |el| result << yield(el) } if block_given?
-        result
+        my_reduce(1) { |_acc, el| result << block.call(el) }
       end
 
       # Написать свою функцию my_compact
       def my_compact
         result = MyArray.new
-        my_each { |el| result << el unless el.nil? }
+        my_reduce(1) do |acc, el|
+          result << el unless el.nil?
+          acc
+        end
         result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(acc = nil)
-        array = self
+      def my_reduce(acc = nil, i = nil, array = nil, &block)
+        i ||= 0
+        array ||= self
+        return acc unless i < array.length
 
         unless acc.present?
           acc = self[0] # first element taken as acc
           array = drop(1) # don't count first element
         end
 
-        array.my_each { |el| acc = yield(acc, el) }
-        acc
+        acc = yield(acc, array[i])
+        my_reduce(acc, i += 1, array, &block)
       end
     end
   end
