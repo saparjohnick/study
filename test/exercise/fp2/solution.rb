89497023cc9ba008a71dc580e7a25b53ed3b91eb
sapar <sapar@meyvndigital.co.uk>
2021-11-06 23:33:05 +0300

    refactoring my_each and my_reduce

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,11 +5,14 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each(&block)
-        for num in self
-          block.call(num)
-        end
-        self
+      def my_each(array = nil, &block)
+        array ||= self
+        starts_at ||= 0
+        return self if array.size == 0
+
+        block.call(array.first)
+        starts_at += 1
+        my_each(array[starts_at..array.size-1], &block)
       end
 
       # Написать свою функцию my_map
@@ -29,15 +32,15 @@ module Exercise
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(acc = nil, &block)
-        my_each do |num|
-          if acc.nil?
-            acc = num
-            next
-          end
-          acc = block.call(acc, num)
-        end
-        acc
+      def my_reduce(acc = nil, array = nil, &block)
+        starts_at ||= 0
+        array ||= self
+        return acc if array.size == 0
+
+        acc, *array = array if acc.nil?
+        acc = yield(acc, array.first)
+        starts_at += 1
+        my_reduce(acc, array[starts_at..array.size-1], &block)
       end
     end
   end
